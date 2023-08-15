7d021c597a9d073376ca777139f0db3b38bf3c7b
Igor Milosavljevic <migor@live.co.uk>
2023-08-15 15:29:56 +0100

    Title update

--- src/components/Buttons/ShinyButton.jsx
+++
@@ -1,8 +0,0 @@
-//button that expands on hover, changes colour and has a light sweep!
-import React from 'react'
-
-const ShinyButton = () => {
-  return <div>ShinyButton</div>
-}
-
-export default ShinyButton
